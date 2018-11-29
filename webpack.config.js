c84bb539ea02a2f24ce34525c600584a505d5052
Kankuro <4katsuki.pain@gmail.com>
2018-11-30 00:07:42 +0800

    Added some options

--- webpack.config.js
+++ webpack.config.js
@@ -135,9 +135,7 @@ const config = {
                     name(module) {
                         // get the name. E.g. node_modules/packageName/not/this/part.js
                         // or node_modules/packageName
-                        const packageName = module.context.match(
-                            /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
-                        )[1];
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
 
                         // npm package names are URL-safe, but some servers don't like @ symbols
                         return `npm.${packageName.replace('@', '')}`;
